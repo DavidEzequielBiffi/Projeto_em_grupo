73454aba52a630d41b588c9c173fdbe94cadbdee
DavidEzequielBiffi <107196319+DavidEzequielBiffi@users.noreply.github.com>
2024-08-23 09:44:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+
+
+
+const perguntas = [
+    {
+        enunciado: "Você é mais otimista ou pessimista?",
+        alternativas: [
+            {
+                texto: " Otimista",
+                afirmacao: " Você é uma pessoa trabalhadora! ",
+            },
+            {
+                texto: " Pessimista ",
+                afirmacao: " Você é uma pessoa preguiçosa! ",
+            }
+        ]
+    },
+    {
+        enunciado: "As pessoas normalmente se irritam com você?",
+        alternativas: [
+            {
+                texto: " Sim ",
+                afirmacao: " Você é uma pessoa otimista que comunica-se bem e fala bastante. ",
+            },
+            {
+                texto: " Não ",
+                afirmacao: " Você é uma pessoa mais séria que não fala muito. ",
+            }
+        ]
+    },
+    {
+        enunciado: "Você mora em:",
+        alternativas: [
+            {
+                texto: " Uma casa ",
+                afirmacao: " VOcê gosta de se divertir no jardim. ",
+            },
+            {
+                texto: " Um apartamento  ",
+                afirmacao: " Você prerfere assistir filmes ao invés de sair de seu apartamento. ",
+            }
+        ]
+    },
+    {
+        enunciado: "Você tem bastante amigos?",
+        alternativas: [
+            {
+                texto: " Sim ",
+                afirmacao: " Você é uma pessoa engraçada. ",
+            },
+            {
+                texto: " Não ",
+                afirmacao: " Você prefere ficar sozinho. ",
+            }
+        ]
+    },
+    {
+        enunciado: "Na maior parte do tempo, você:",
+        alternativas: [
+            {
+                texto: " Trabalha ",
+                afirmacao: "Você é o Bob Esponja!",
+            },
+            {
+                texto: " Dorme ",
+                afirmacao: " Você é o Patrick Estrela! ",
+            }
+        ]
+    },
+];
