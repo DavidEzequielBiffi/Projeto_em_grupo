5b2785914aeff1b18e2ac07e37e8585ef3a8a9a6
DavidEzequielBiffi <107196319+DavidEzequielBiffi@users.noreply.github.com>
2024-08-30 10:44:52 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -77,10 +77,18 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = ""; 
 
 function mostraPergunta() {
+    if(atual >= perguntas.length){
+mostraResultado();
+return;
+
+    }
+
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
 
@@ -88,8 +96,22 @@ function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click",() => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
+}
+
+function respostaSelecionada(opcaoSeleionada){
+    const afirmacoes = opcaoSeleionada.afirmacoes;
+    historiaFinal += afirmacoes;
+    atual++                     
+    mostraPergunta();
+}
+
+function mostraResultado(){
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
 }
 
 mostraPergunta();
